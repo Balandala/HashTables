38ab3f16554b44741381764628e60e7dd04848ce
Balandala <balandala05@gmail.com>
2024-12-17 23:37:53 +0500

    added project struct

--- HashTables/Program.cs
+++ HashTables/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Program;
+
+public static class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
